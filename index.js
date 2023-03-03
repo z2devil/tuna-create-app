5ba03c332711e4283b8d1d6b21b2ec73eddc3c6b
z2devil <cn.z2devil@gmail.com>
2023-03-03 15:51:10 +0800

    feat: 项目更名

--- index.js
+++ index.js
@@ -1,50 +1,91 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path');
+import dotenv from 'dotenv';
+import axios from 'axios';
+import { program } from 'commander';
+import prompts from 'prompts';
+import ora from 'ora';
+
+// import path from path;
+// import fs from fs;
+// import util from util;
+// import { ChildProcess } from 'child_process';
+// const exec = util.promisify(ChildProcess.exec);
+
+dotenv.config();
+
+const service = axios.create({
+  baseURL: `${process.env.BASE_PATH}/${process.env.REPOS}/contents/`,
+  timeout: 30000,
+});
+
+const spinner = ora();
+
+const loadTemplates = async () => {
+  spinner.start('正在加载模板列表...');
+  try {
+    const { data: templates } = await service.get(process.env.ROOT + '1');
+    spinner.stop();
+    return templates;
+  } catch (error) {
+    spinner.stop();
+    const { retry } = await prompts({
+      type: 'confirm',
+      name: 'retry',
+      message: '加载模板列表失败，是否重试？',
+      initial: true,
+    });
+    if (retry) {
+      return loadTemplates();
+    }
+    process.exit();
+  }
+};
+
+program.version(process.env.VERSION);
 
 program
-  .version('0.0.1')
-  .description('A simple CLI for creating a new project')
-  .option('-n, --name <name>', 'Project name')
-  .option('-d, --directory <directory>', 'Project directory')
-  .option('-f, --force', 'Overwrite existing files')
-  .parse(process.argv);
+  .command(process.env.COMMAND)
+  .description('Create a new app')
+  .action(async () => {
+    const { projectName } = await prompts({
+      type: 'text',
+      name: 'projectName',
+      message: '请输入项目名称：',
+      initial: 'my-app',
+      validate: name => {
+        if (name === '') {
+          return '项目名称不可为空';
+        }
+        return true;
+      },
+    });
 
-const projectName = program.name || 'my-app';
-const projectDirectory = program.directory || projectName;
-const projectPath = path.join(process.cwd(), projectDirectory);
+    const templates = await loadTemplates();
 
-if (fs.existsSync(projectPath) && !program.force) {
-  console.error(`Project directory ${projectDirectory} already exists!`);
-  process.exit(1);
-}
+    const templateNames = templates
+      .filter(template => template.type === 'dir')
+      .map(template => template.name);
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'language',
-      message: 'Select a programming language:',
-      choices: ['JavaScript', 'TypeScript'],
-    },
-    {
-      type: 'confirm',
-      name: 'useLinter',
-      message: 'Would you like to use a linter?',
-      default: true,
-    },
-  ])
-  .then((answers) => {
-    console.log('Creating project...');
-    console.log(`Project name: ${projectName}`);
-    console.log(`Project directory: ${projectDirectory}`);
-    console.log(`Programming language: ${answers.language}`);
-    console.log(`Use a linter: ${answers.useLinter}`);
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
+    const { selectedTemplateName } = await prompts({
+      type: 'select',
+      name: 'selectedTemplateName',
+      message: '请选择模板：',
+      choices: templateNames,
+    });
+
+    // const appPath = path.join(process.cwd(), name);
+    // fs.mkdirSync(appPath);
+    // process.chdir(appPath);
+
+    // console.log(`Downloading ${selectedTemplateName} template...`);
+
+    // await exec(`npx degit ${depot}/${root}/${selectedTemplateName}#${selectedFileNames.join(',')} .`, { stdio: 'inherit' });
+
+    // console.log('Installing dependencies...');
+    // await exec('npm install', { stdio: 'inherit' });
+
+    // console.log('Done!');
   });
+
+program.parse(process.argv);
